2a4600dbd40f4ea4074c3f88a2b4115c29254149
Karen Agustina Soto <karenagustinams@gmail.com>
2022-04-17 22:08:20 -0300

    BaSP-W05-2022: Corrections in README

--- semana-05/exercises/01_variables_and_operators.js
+++ semana-05/exercises/01_variables_and_operators.js
@@ -1,1 +1,22 @@
-console.log('Testing JS Script')
+console.log('Testing JS Script');
+
+console.log('Variables and operators');
+
+console.log('EXERCISE 1.a');
+
+var num1 = 5;
+var num2 = 10;
+var sum = num1 + num2;
+console.log(sum);
+
+console.log('EXERCISE 1.b');
+var firstString = 'This is ';
+var secondString = 'a String.';
+var sentence = firstString + secondString;
+console.log(sentence);
+
+console.log('EXERCISE 1.c');
+var thirdString = 'I am ';
+var fourthString = 'another String.';
+var secondSentence = thirdString.length + fourthString.length;
+console.log(secondSentence);
