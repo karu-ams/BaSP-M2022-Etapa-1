b2cdde39ba8900da200514a7802393921d5e7acb
Karen Agustina Soto <karenagustinams@gmail.com>
2022-05-02 14:01:16 -0300

    BaSP: Email validate in login succesfully created

--- semana-07/javascript/login.js
+++ semana-07/javascript/login.js
@@ -1,31 +1,45 @@
 window.onload = function() {
+    // OBJECTS
     var email = document.getElementById('email');
     var password = document.getElementById('password');
-    var submitButton = document.getElementById('submit') ;
+    var submitButton = document.getElementById('submit');
+
+    // ERROR
+    var errorEmail = document.getElementById('email-error')
 
     // VARIABLES
     var emailFormat = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
-    var emailAlert = document.createElement('p');
     
     // EVENTS //
-    email.onblur = validateEmail;
-    /*submitButton.addEventListener('click', validateLoginForm);*/
-    /*email.addEventListener('blur', validateEmail);*/
-    /*email.addEventListener('focus', inFocusEmail);*/
-    /*password.addEventListener('blur', validatePassword);*/
-    /*password.addEventListener('focus', inFocusPwd);*/
+    email.addEventListener('blur', validateEmail);
+    email.addEventListener('focus', emailFocus)
 
     // FUNCTIONS //
-    function validateEmail(email){
-        var isValid = emailFormat.test((email.value));
+   /* email.onblur = function () {
+        var isValid = emailFormat.test(email.value);
         if(isValid == true){
             alert('Valid email.')
         } else {
             alert('Sorry. The email is not valid.')
         }
-        /*return emailFormat.test(email) ? true : false;*/
     }
-    console.log(emailFormat.test(email));
-    email.addEventListener('blur', validateEmail);
+    console.log(emailFormat.test(email.value));*/
+
+    function validateEmail () {
+        var isValid = emailFormat.test(email.value);
+        if(isValid == true){
+            console.log('Valid email.')
+            errorEmail.classList.add('error-inv')
+            errorEmail.classList.replace('error-input','error-inv')
+        } else {
+            console.log('Sorry. The email is not valid.')
+            errorEmail.classList.add('error-input')
+            errorEmail.classList.replace('error-inv', 'error-input')
+        }
+    }
+
+    function emailFocus () {
+        errorEmail.classList.replace('error-input','error-inv')
+    }
 }
 
