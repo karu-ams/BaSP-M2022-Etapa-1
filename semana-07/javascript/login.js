a6ef8dfb7769763ff6e01f082d17f8e766a47997
Karen Agustina Soto <karenagustinams@gmail.com>
2022-05-03 19:47:42 -0300

    BaSP-W07-2022: Styles in login added for errors

--- semana-07/javascript/login.js
+++ semana-07/javascript/login.js
@@ -1,14 +1,14 @@
 window.onload = function() {
-    // OBJECTS
+    // OBJECTS //
     var email = document.getElementById('email');
     var password = document.getElementById('password');
     var submitButton = document.getElementById('submit');
 
-    // ERROR
-    var errorEmail = document.getElementById('email-error')
-    var errorPassword = document.getElementById('password-error')
+    // ERROR ALERTS //
+    var errorEmail = document.getElementById('email-error');
+    var errorPassword = document.getElementById('password-error');
 
-    // VARIABLES
+    // VARIABLES //
     var emailFormat = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
     
     // EVENTS //
@@ -18,7 +18,8 @@ window.onload = function() {
     password.addEventListener('focus', passwordFocus);
     submitButton.addEventListener('click', submitLogin);
 
-    // FUNCTIONS //
+    // FUNCTIONS // 
+    // ANOTHER OPTION
    /* email.onblur = function () {
         var isValid = emailFormat.test(email.value);
         if(isValid == true){
@@ -29,53 +30,54 @@ window.onload = function() {
     }
     console.log(emailFormat.test(email.value));*/
 
-    function validateEmail () {
+    function validateEmail() {
         var isValid = emailFormat.test(email.value);
         if(isValid == true){
-            console.log('Valid email.') /// ver de sacar
             errorEmail.classList.add('error-inv');
             return true;
-           // errorEmail.classList.replace('error-input','error-inv')
         } else {
-            console.log('Sorry. The email is not valid.') /// ver de sacar
+            console.log('Sorry. The email is not valid.'); /// ver de sacar
             errorEmail.classList.add('error-input');
             return false;
-          //  errorEmail.classList.replace('error-inv', 'error-input')
         }
     }
 
-    function emailFocus () {
+    function emailFocus() {
         errorEmail.classList.replace('error-input','error-inv');
     }
 
-    ///// PASSWORD FUNCTION
-    function validatePassword () {
+    // PASSWORD FUNCTION //
+    function validatePassword() {
         if (haveNumAndLett(password.value) && password.value.length >= 8) {
-            console.log('correct Password')
+            console.log('correct Password');
             /*errorPassword.classList.add('error-inv')*/
             return true;
         } else {
-            console.log('incorrect pass.')
+            console.log('incorrect pass.');
             errorPassword.classList.add('error-input');
             return false;
         }
     }
 
-    function passwordFocus () {
-        errorPassword.classList.replace('error-input','error-inv')
+    function passwordFocus() {
+        errorPassword.classList.replace('error-input','error-inv');
     }
 
-    /////// LOG IN
-    function submitLogin () {
+    // LOG IN //
+    function submitLogin() {
         if (validateEmail() && validatePassword()){
-            alert('The mail is: '+email.value+'\nThe password is: '+password.value)
+            var url = 'https://basp-m2022-api-rest-server.herokuapp.com/login';
+            var keylist = ['email', 'password'];
+            var valuelist = [email.value, password.value];
+            fetchLogin(url, keylist, valuelist);
+            alert('The mail is: '+email.value+'\nThe password is: '+password.value);
         }
     }
 
-    /////// AUXILIARES
-    function haveNumAndLett (str) {
-        var letters = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
-        var numbers = '1234567890'
+    // AUXILIARES //
+    function haveNumAndLett(str) {
+        var letters = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZáéíóúÁÉÍÓÚ';
+        var numbers = '1234567890';
         var areLetters = false;
         var areNumbers = false;
         for (var i = 0; i < str.length ; i++) {
@@ -88,6 +90,26 @@ window.onload = function() {
             }
         }
         return (areLetters && areNumbers);
+    }
+
+    function fetchLogin(url, keylist, valuelist) {
+        var keyValueArr = []
+        for (var i = 0; i < keylist.length ; i++) {
+            keyValueArr.push(keylist[i].concat('=', valuelist[i]));
+        }
+        var queryParams = keyValueArr.join('&');
+        var fetchURL = url.concat('?', queryParams);
+
+        fetch (fetchURL)
+            .then(function(response){ ///tiene que tener un then que transforme a json el obj
+                return response.json();
+            })
+            .then(function(jsonResponse){ //// agregar IF para que me de mensaje success
+                alert(jsonResponse.msg);
+            })
+            .catch(function(error){ ///siempre termina con catch
+                console.log(error);
+            })
     }
 }
 
